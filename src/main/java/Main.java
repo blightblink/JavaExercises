a60dc1d7cae5e906bc8ae2e25d0e455da2c10a4f
Chatzigeorgiou Xenofontas <chatzigeorgiou@intralot.com>
2020-03-02 20:52:17 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(myFirstTest());
+    }
+
+    /**Will choose an exercise later
+     *
+     * @return x
+     */
+    public static Integer myFirstTest(){
+        Integer x = null;
+
+        //Write your code here
+
+        return x;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void myFirstTestTest(){
+        Assert.assertNull(Main.myFirstTest());
+
+    }
+}
