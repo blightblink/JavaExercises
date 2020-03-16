1a6fd63ff3e50c388a6114fbe2b725a5150a1cf0
Chatzigeorgiou Xenofontas <chatzigeorgiou@intralot.com>
2020-03-16 18:02:02 +0200

    First test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,37 @@
+import java.util.Random;
+
 public class Main {
+    private static Random rand = new Random();
+
     public static void main(String[] args) {
-        System.out.println(myFirstTest());
+        System.out.println(eldrichBlast());
     }
 
-    /**Will choose an exercise later
+    /** Create a function that returns the damage of an eldrich blast cast by a lvl 5 WARLOCK !!
      *
+     * use the power of the almighty dice generator( @rollADice ) provided by the gm ( @Me )
      * @return x
      */
-    public static Integer myFirstTest(){
+    public static Integer eldrichBlast(){
         Integer x = null;
 
         //Write your code here
 
         return x;
     }
+
+    /**
+     *  Only me can touch this, please
+     * @param dice
+     * @return random number from 0 to x
+     */
+    public static Integer rollADice(Integer dice) {
+        if(dice <= 0) {
+            return 0;
+        }
+        return rand.nextInt(dice);
+    }
+
+
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,11 +1,38 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Random;
 
 public class MainTest {
 
+    public class mockRandom extends Random{
+
+        @Override
+        public int nextInt(){
+            return 0;
+        }
+    }
+
+    @Mock
+    mockRandom mockRandom;
+
+    @Before
+    public void before(){
+    }
+
+
     @Test
     public void myFirstTestTest(){
-        Assert.assertNull(Main.myFirstTest());
+        Assert.assertNotNull(Main.eldrichBlast());
 
+        int x = 0;
+        for(int i = 0 ; i < 1000 ; i++){
+            x = Main.eldrichBlast();
+            Assert.assertTrue("Wrong number !! you had a: " + x  ,x > 1);
+            Assert.assertTrue("Wrong number !! you had a: " + x  ,x < 21);
+        }
     }
 }
